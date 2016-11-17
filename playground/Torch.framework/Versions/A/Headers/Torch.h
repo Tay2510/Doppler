708fe4869501895bdb66410d4133ff33cb247b93
Jeremy, Yen <r00b46024@ntu.edu.tw>
2016-11-17 17:50:57 -0500

    update

---
+++ playground/Torch.framework/Versions/A/Headers/Torch.h
@@ -0,0 +1,16 @@
+#ifndef TORCH_IOS_FRAMEWORK_TORCH_H
+#define TORCH_IOS_FRAMEWORK_TORCH_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "TH/TH.h"
+#include "lua.h"
+#include "luaconf.h"
+#include "lauxlib.h"
+#include "luaT.h"
+#include "lualib.h"
+#ifdef __cplusplus
+}
+#endif
+#endif
+

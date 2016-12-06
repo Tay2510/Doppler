12c3f838bb49c2d93d4345cd95d788c8b5632b82
Jeremy, Yen <r00b46024@ntu.edu.tw>
2016-12-05 23:57:21 -0500

    update code

---
+++ Doppler/Doppler/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Doppler
+//
+//  Created by Tay on 2016/12/5.
+//  Copyright © 2016年 CMU. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
